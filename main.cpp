b9b80bd2efb3ca4873b63d564f4db431134ce460
fog <fog.hua@gmail.com>
2021-07-15 10:22:44 +0800

    ! update files

--- main.cpp
+++ main.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
+
+#ifdef STATIC_BUILD
 #include "std_dllapi.h"
+#else
+#ifdef __cplusplus
+extern "C"{
+#endif
 
-//#ifdef __cplusplus
-//extern "C"{
-//#endif
+void PrScrn();
 
-//void PrScrn();
+#ifdef __cplusplus
+}
+#endif
 
-//#ifdef __cplusplus
-//}
-//#endif
+#include <windows.h>
+#include <gdiplus.h>
+
+#endif
 
 using namespace std;
 
 int main()
 {
-//    PrScrn();
+    // prevent from command console popup
+    FreeConsole();
+
+#ifdef STATIC_BUILD
     Function<void> f("PrScrn.dll", "PrScrn");
-
     f();
+#else
+    ULONG_PTR m_gdiplusToken;   // class member
 
-    cout << "Hello World!" << endl;
+    // InitInstance
+    Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+    Gdiplus::GdiplusStartup(&m_gdiplusToken, &gdiplusStartupInput, NULL);
+
+    PrScrn();
+
+    // ExitInstance
+    Gdiplus::GdiplusShutdown(m_gdiplusToken);
+#endif
+
     return 0;
 }
